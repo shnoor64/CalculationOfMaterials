c1552b1cfb330c0a2fbd2cdf748d2650985d4f15
Shnoor64 <shnooor64@gmail.com>
2021-07-01 13:35:46 +0300

    First commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,67 @@
+import entity.PriceList;
+
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 
 public class Main {
+    public static void main(String[] args) {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String input = "";
+        try {
+            while (!input.equalsIgnoreCase("stop")) {
+                showMenu();
+                input = in.readLine();
+                if (input.equals("add")) {
+                    System.out.println("Введите путь к файлу");
+                    String filePathXls = in.readLine();
+                    File file = new File(filePathXls);
+                    if (!file.exists()) {
+                        System.out.println(file.getCanonicalPath() + "- указанный путь не существует, проверьте правильность ввода");
+                        continue;
+                    }
 
-    public static void main(String... args) throws IOException {
-        SelectorScenario.selectingScenario(args);
+                    System.out.println("Введите поставщика");
+                    String provider = in.readLine();
+                    System.out.println("Введите дату обновления");
+                    String updateDate = in.readLine();
+                    Recorder.recordInDatabase(filePathXls, provider, updateDate);
+                } else if (input.equals("search")) {
+                    System.out.println("Что ищем?");
+                    String searchWord = in.readLine();
+                    System.out.println("Результат поиска:");
+                    List<PriceList> resoult = Searcher.searchByWord(searchWord);
+                    if (resoult.isEmpty()) {
+                        System.out.println("По запросу <"+searchWord+"> ничего не найдено");
+                    }
+                    else {
+                        AveragePrice.calculatingAveragePrice(resoult);
+                    }
+                } else if (input.equals("remove")) {
+                    System.out.println("Введите имя поставщика, прайслист которого хотите удалить");
+                    String nameProvider = in.readLine();
+                    Remover.RemoverFromDatabase(nameProvider);
+                    System.out.println("Прайслист поставщика <"+nameProvider+"> успешно удален");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public static void showMenu() {
+        System.out.println("Введите команду:" + "\n" +
+                "add для добавления нового прайслиста" + "\n" +
+                "search для поиска" + "\n" +
+                "remove для удаления прайслиста" + "\n" +
+                "stop для выхода");
+
+//////////////////////////
+//    public static void main(String... args) throws IOException {
+//        SelectorScenario.selectingScenario(args);
+///////////////////////////
 
 //        Recorder.recordInDatabase("C:\\Users\\Shnoor64\\IdeaProjects\\CalculationOfMaterials\\testPrice\\testfile1.xls"
 //                ,"ИП Стройхлам"
--- src/main/java/SelectorScenario.java
+++ src/main/java/SelectorScenario.java
@@ -1,16 +1,11 @@
 public class SelectorScenario {
     public static void selectingScenario(String[] args) {
-        switch (args [0]){
-            case "add":
-                Recorder.recordInDatabase(args [1],args [2] ,args [3]);
-                break;
-            case "search":
-                AveragePrice.calculatingAveragePrice(Searcher.searchByWord(args [1]));
-                break;
-            case "remove":
-                Remover.RemoverFromDatabase(args [1]);
-                break;
-
+        if ("add".equals(args[0])) {
+            Recorder.recordInDatabase(args[1], args[2], args[3]);
+        } else if ("search".equals(args[0])) {
+            AveragePrice.calculatingAveragePrice(Searcher.searchByWord(args[1]));
+        } else if ("remove".equals(args[0])) {
+            Remover.RemoverFromDatabase(args[1]);
         }
 
     }
